6ee92f1ae4145c5faa6da163f68f6dee92d148d3
Berta González <berta.gonzalez99@gmail.com>
2026-08-14 18:42:18 +0200

    feat(tmdb.js): Call TMDB API with axios

---
+++ src/features/movies/services/tmdbAPI.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const API_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const tmdb = axios.create({
+  baseURL: API_URL,
+  params:{
+    api_key: API_KEY,
+    language: 'es-ES',
+  },
+})
+
+export async function getPopularMovies(){
+  const response = await tmdb.get('/movie/popular')
+  return response.data.results
+}
+
+export async function searchMovies(query){
+  const response = await tmdb.get('/search/movie',{
+    params: { query },
+  })
+  return response.data.results
+}
