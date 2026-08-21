a160f8cf06426fb14ba52378ace848cd4a805d86
Berta González <berta.gonzalez99@gmail.com>
2026-08-21 13:09:34 +0200

    feat: Add movie and credits types

--- src/features/movies/types/movies.types.ts
+++ src/features/movies/types/movies.types.ts
@@ -1,8 +1,18 @@
-export interface Movie {
+export interface Movie{
   id: number
   title: string
   poster_path: string | null
   release_date: string
   vote_average: number
   overview: string
+}
+
+export interface Genre{
+  id: number
+  name: string
+}
+
+export interface MovieDetails extends Movie{
+  runtime: number
+  genres: Genre[]
 }
